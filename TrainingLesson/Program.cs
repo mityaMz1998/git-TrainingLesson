7e676aa8966d8b034fec75aef05feffb1f1067dc
Дмитрий Мазин <mitya.mazin@mail.ru>
2023-04-22 11:50:17 +0300

    Added !!!

--- TrainingLesson/Program.cs
+++ TrainingLesson/Program.cs
@@ -6,7 +6,7 @@ namespace TrainingLesson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
